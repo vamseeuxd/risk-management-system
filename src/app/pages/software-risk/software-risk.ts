15d94efa86aecfec8219799bf86a2262ca94b8f2
vamseeuxd <vamsee.uxd@gmail.com>
2025-01-30 07:19:35 +0530

    feat: implement software security analysis feature with UI enhancements and API integration

--- src/app/pages/software-risk/software-risk.ts
+++ src/app/pages/software-risk/software-risk.ts
@@ -1,29 +1,47 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { PopoverController } from '@ionic/angular';
+import { LoadingController, PopoverController } from "@ionic/angular";
 
-import { PopoverPage } from '../about-popover/about-popover';
+import { PopoverPage } from "../about-popover/about-popover";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'page-software-risk',
-  templateUrl: 'software-risk.html',
-  styleUrls: ['./software-risk.scss'],
+  selector: "page-software-risk",
+  templateUrl: "software-risk.html",
+  styleUrls: ["./software-risk.scss"],
 })
 export class SoftwareRiskPage {
-  location = 'madison';
-  conferenceDate = '2047-05-17';
-
-  selectOptions = {
-    header: 'Select a Location'
-  };
-
-  constructor(public popoverCtrl: PopoverController) { }
-
-  async presentPopover(event: Event) {
-    const popover = await this.popoverCtrl.create({
-      component: PopoverPage,
-      event
+  softwareName = "";
+  isModalOpen = false;
+  analysis = "";
+  risk_level = "";
+  constructor(public http: HttpClient, public loadingCtrl: LoadingController) {}
+  async analyzeSecurity() {
+    this.analysis = "";
+    this.risk_level = "";
+    const loading = await this.loadingCtrl.create({
+      message: "Please wait while we analyze the security of the software...",
+      duration: 2000,
     });
-    await popover.present();
+
+    loading.present();
+    setTimeout(() => {
+      this.analysis =
+        "Cognizant has faced several security breaches, including a ransomware attack in 2020 that affected its operations.";
+      this.risk_level =
+        "These incidents highlight the need for Cognizant to enhance its cybersecurity measures to protect client and employee data.";
+      this.isModalOpen = true;
+      loading.dismiss();
+    }, 2000);
+    this.http
+      .post(
+        "https://97e2-2406-7400-1c3-6062-c91a-dbe0-4c3-7428.ngrok-free.app/analyze_security",
+        {
+          text: "cognizant",
+        }
+      )
+      .subscribe((data) => {
+        console.log(data);
+      });
   }
 }
